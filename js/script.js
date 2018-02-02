9c59d21e76b4fa57f910d6980dbfa8fdcaf96a45
github username <github email address>
2018-02-02 14:00:53 -0500

    now trying

--- js/script.js
+++ js/script.js
@@ -7,8 +7,11 @@ $(document).ready(function(){
 
   $('.course .delete').on('click', function(event){
     alert("about to delete");
+
+
     event.stopPropagation();
   });
+  // This is a change
 
   //stop propagation
   // $('.course').on('click', function(){
